b6b316797ad9ee6b982113da1b339df78e20e9c3
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-06-06 23:49:43 +0900

    statefull

--- lib/main.dart
+++ lib/main.dart
@@ -6,41 +6,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo!',
+      title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          // 左側のアイコン
-          leading: Icon(Icons.arrow_back),
-          // タイトルテキスト
-          title: Text('Hello'),
-          // 右側のアイコン一覧
-          actions: <Widget>[
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.favorite),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.more_vert),
-            ),
-          ],
-        ),
         body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              ListView(
-                children: <Widget>[
-                  Text('Item 1'),
-                  Text('Item 2'),
-                  Text('Item 3'),
-                ],
-              )
-            ],
-          ),
+          child: MyWidget(),
         ),
       ),
+    );
+  }
+}
+
+// StatefulWidgetを継承するとStateを扱える
+// このWidgetを表示すると、Stateを元にUIが作成される
+class MyWidget extends StatefulWidget {
+  // 使用するStateを指定
+  @override
+  _MyWidgetState createState() => _MyWidgetState();
+}
+
+// Stateを継承して使う
+class _MyWidgetState extends State<MyWidget> {
+  // データを宣言
+  int count = 0;
+
+  // データを元にWidgetを作る
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(count.toString()),
+        TextButton(
+          onPressed: () {
+            // データを更新する時は setState を呼ぶ
+            setState(() {
+              // データを更新
+              count = count + 1;
+            });
+          },
+          child: Text('カウントアップ'),
+        ),
+      ],
     );
   }
 }
